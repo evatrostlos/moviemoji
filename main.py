d1e19cff88b1dc424b2a58a0a74d935337f70791
alicetragedy <girl@alicetragedy.org>
2016-09-10 17:08:47 +0200

    add one movie to play method, remove unnecessary methods

--- main.py
+++ main.py
@@ -14,28 +14,20 @@ logger = logging.getLogger(__name__)
 # Methods handling commands
 
 def start(bot, update):
-    bot.sendMessage(chat_id=update.message.chat_id,
+  bot.sendMessage(chat_id=update.message.chat_id,
                     text="Hello, would you like to play a game? I'm going to send you emojis and you tell me what movie title they refer to. Ok? If you ever need help, type /help!")
 
-def hello(bot, update):
-    bot.sendMessage(chat_id=update.message.chat_id,
-                    text='Hello {}'.format(update.message.from_user.first_name))
-
 def help(bot, update):
-    bot.sendMessage(update.message.chat_id, text='To start a new game, type /start. To get help, type /help. To end the current round and start a new one, type /next.')
+  bot.sendMessage(update.message.chat_id, text='To start a new game, type /play. To get help, type /help. To end the current round and start a new one, type /next.')
 
-def echo(bot, update):
-    bot.sendMessage(chat_id=update.message.chat_id, text=update.message.text)
+def play(bot, update):
+  scream3 = "😱😱😱"
+  bot.sendMessage(update.message.chat_id, text= scream3)
 
-def caps(bot, update, args):
-    text_caps = ' '.join(args).upper()
-    bot.sendMessage(chat_id=update.message.chat_id, text=text_caps)
 
 
 # Helpers
 
-echo_handler = MessageHandler([Filters.text], echo)
-caps_handler = CommandHandler('caps', caps, pass_args=True)
 
 updater = Updater(config.TELEGRAM_SECRET_KEY)
 
@@ -44,10 +36,9 @@ dispatcher = updater.dispatcher
 
 # Register the methods handling commands
 dispatcher.add_handler(CommandHandler('start', start))
-dispatcher.add_handler(CommandHandler('hello', hello))
 dispatcher.add_handler(CommandHandler('help', help))
-dispatcher.add_handler(echo_handler)
-dispatcher.add_handler(caps_handler)
+dispatcher.add_handler(CommandHandler('play', play))
+dispatcher.add_handler(CommandHandler('next', next))
 
 updater.start_polling()
 updater.idle()
